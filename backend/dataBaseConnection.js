6e5d65a49c109b49e036f0bc91dba021a53def9c
papaousmane21 <papaousmanediene98@gmail.com>
2023-11-24 19:34:19 +0100

    modifications des parametres de connexion to DB

--- backend/dataBaseConnection.js
+++ backend/dataBaseConnection.js
@@ -3,11 +3,11 @@ const env = require("dotenv").config();
 
 const connectToDatabase = () => {
   const connectionInfo = {
-    host: "localhost", //process.env.DB_HOST,
-    user: "root", //process.env.DB_USER,
-    password: "123456",
-    database: "thankstip",
-    port: 3306,
+    host: process.env.DB_HOST, //,
+    user: process.env.DB_USER, //process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
   };
 
   return (connection = mysql.createConnection(connectionInfo));
