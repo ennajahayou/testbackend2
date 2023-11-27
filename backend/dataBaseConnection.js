6df6d57baed5d4170422659e3da8a6e9742a4abd
Hrafbh <achrafbahich0@gmail.com>
2023-11-27 12:46:28 +0100

    add port

--- backend/dataBaseConnection.js
+++ backend/dataBaseConnection.js
@@ -6,7 +6,8 @@ const connectToDatabase = () => {
     host: process.env.DB_HOST, //,
     user: process.env.DB_USER, //process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.PORT
 
   };
 
