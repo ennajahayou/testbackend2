8c1da231b2128f03cf8baa567c8661a9cf108485
papaousmane21 <papaousmanediene98@gmail.com>
2023-11-28 15:39:13 +0100

    database file  update

--- backend/dataBaseConnection.js
+++ backend/dataBaseConnection.js
@@ -3,10 +3,10 @@ const env = require("dotenv").config();
 
 const connectToDatabase = () => {
   const connectionInfo = {
-    host: "localhost", //process.env.DB_HOST,
-    user: "root", //process.env.DB_USER,
-    password: "123456",
-    database: "thankstip",
+    host: process.env.DB_HOST, //process.env.DB_HOST,
+    user: process.env.DB_USER, //process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: 3306,
   };
 
