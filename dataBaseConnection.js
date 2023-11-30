dcb6e60b35367256fd8c250f36916a179f09b553
ennajahayou <95098812+ennajahayou@users.noreply.github.com>
2023-11-30 15:30:59 +0100

    Update dataBaseConnection.js

--- dataBaseConnection.js
+++ dataBaseConnection.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql2");
-// const env = require("dotenv").config();
+const env = require("dotenv").config();
 
 const connectToDatabase = () => {
   const connectionInfo = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: 'databaseayoub.censrelaamzf.eu-west-3.rds.amazonaws.com', //process.env.DB_HOST,
+    user: "admin", //process.env.DB_USER,
+    password:'Azert12345',
+    database: 'backendbase',
+    port: 3307
   };
 
   return (connection = mysql.createConnection(connectionInfo));
