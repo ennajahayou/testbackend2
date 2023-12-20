796078fac5d33f8703ef39854af9254a82935b02
papaousmane21 <papaousmanediene98@gmail.com>
2023-12-20 10:27:41 +0100

    modification du fichier database... pour le compteur

--- dataBaseConnection.js
+++ dataBaseConnection.js
@@ -1,16 +1,47 @@
-const mysql = require("mysql2");
-const env = require("dotenv").config();
+/*const mysql = require("mysql2");
+//const env = require("dotenv").config();
 
 const connectToDatabase = () => {
-  const connectionInfo = {
-    host: 'database-testdeploy.censrelaamzf.eu-west-3.rds.amazonaws.com', //process.env.DB_HOST,
-    user: "admin", //process.env.DB_USER,
-    password:'Azert12345',
-    database: 'database_testdeploy',
-    port: 3307
+    const connectionInfo = {
+      host: process.env.DB_HOST, //process.env.DB_HOST,
+      user: process.env.DB_USER, //process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 3307,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    };
+
+    return (connection = mysql.createConnection(connectionInfo));
   };
 
-  return (connection = mysql.createConnection(connectionInfo));
+module.exports = connectToDatabase;*/
+
+const mysql = require("mysql2");
+const env = require("dotenv").config();
+const connectionInfo = {
+  host: process.env.DB_HOST, //process.env.DB_HOST,
+  user: process.env.DB_USER, //process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 };
+const connectToDatabase = () => {
+  return mysql.createConnection(connectionInfo);
+};
+
+function decrementValue() {
+  const pool = mysql.createConnection(connectionInfo)
+  pool.query('UPDATE execution SET remaining_time = remaining_time - 1 WHERE remaining_time IS NOT NULL AND remaining_time > 0', (error, results, fields) => {
+  pool.close()
+    if (error) throw error;
+});
+}
+
+setInterval(decrementValue, 1000);
 
 module.exports = connectToDatabase;
--- routes/countdownTrigger.js
+++ routes/countdownTrigger.js
@@ -3,7 +3,7 @@ const talentsThanksCalculator = require('./talentsThanksCalculator');
 
 const startCountdown = (hours, executionId) => {
     
-    const durationInMilliseconds = hours * 1000;
+    const durationInMilliseconds = hours * 3600 * 1000;
   
     setTimeout(() => {
       // Action à déclencher après le compte à rebours
